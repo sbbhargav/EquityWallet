f5a7d9ef799dcbfd5d2eb8140ab5cfa831cb2951
bhargav <sbbhargav1993@gmail.com>
2022-05-15 12:41:19 +0530

    added dependent in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
 
-	has_many :stocks
+	has_many :stocks, dependent: :destroy
 
 	validates :username, presence: true
 	validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
